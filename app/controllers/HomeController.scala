54d4f48614e98b35753f88828d057ad430217193
ryoya katagiri <ryoya3091@yahoo.co.jp>
2019-03-27 23:12:38 +0900

    Select all by slick

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,26 @@
 package controllers
 
 import javax.inject._
-import play.api._
+import models._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import play.api.i18n._
+import play.api.libs.json.Json
 import play.api.mvc._
+import scala.concurrent.{ ExecutionContext, Future }
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(repository: PersonRepository,
+                               cc: MessagesControllerComponents)
+                              (implicit ec: ExecutionContext) extends MessagesAbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index() = Action.async { implicit request =>
+    repository.list().map { people =>
+      Ok(views.html.index(
+        "People Data.", people
+      ))
+    }
   }
+
 }
--- build.sbt
+++ build.sbt
@@ -9,6 +9,9 @@ scalaVersion := "2.12.8"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.1" % Test
+libraryDependencies += "com.h2database" % "h2" % "1.4.197"
+libraryDependencies += "com.typesafe.play" %% "play-slick" % "3.0.3"
+libraryDependencies += "com.typesafe.play" %% "play-slick-evolutions" % "3.0.3"
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.ryoya.controllers._"
